0878041d2268e48211781addeae577a335606389
Brian Jones <bjones.tech@icloud.com>
2018-06-09 09:18:01 -0400

    Updated header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,20 @@ import React, { Component } from 'react';
 const Header = props => {
   return (
     <div>
-      <h2>Trivia Quiz</h2>
+      <h1 style={titleStyle} className='alfa-font'>Trivia Quiz</h1>
+      <h3 style={subTitleStyle}>Sports & Entertainment</h3>
     </div>
   )
 }
 
 export default Header
+
+// inline styles
+
+const titleStyle = {
+  color: '#408cbf'
+}
+
+const subTitleStyle = {
+  fontWeight: '100'
+}
